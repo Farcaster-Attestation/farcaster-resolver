f38264277fa2905b24708ac2c3e5adcaa27bf628
johny <johny@upnode.org>
2024-07-08 18:14:27 +0700

    change mapping data

--- contracts/FarcasterResolver.sol
+++ contracts/FarcasterResolver.sol
@@ -5,7 +5,9 @@ import { IEAS, Attestation } from "@ethereum-attestation-service/eas-contracts/c
 import { SchemaResolver } from "@ethereum-attestation-service/eas-contracts/contracts/resolver/SchemaResolver.sol";
 
 contract FarcasterResolver is SchemaResolver {
-    mapping(uint256 => address) public fidAttested;
+    // Mapping of key is the keccak256 hash of the farcaster id and the verifier address
+    // The value is the attestation uid
+    mapping(bytes32 => bytes32) public fidAttested;
 
    constructor(IEAS eas) SchemaResolver(eas) {}
 
@@ -14,11 +16,12 @@ contract FarcasterResolver is SchemaResolver {
     /// @param attestation The attestation to add
     function onAttest(Attestation calldata attestation, uint256 /*value*/) internal override returns (bool) {
         (uint256 fid, address verifyAdrress,) = abi.decode(attestation.data, (uint256, address, uint8));
-        if (verifyAdrress == fidAttested[fid]) {
+        bytes32 key = computeKey(fid, verifyAdrress);
+        if (bytes32(0) == fidAttested[key]) {
             return false;
         }
 
-        fidAttested[fid] = verifyAdrress;
+        fidAttested[key] = attestation.uid;
 
         return true;
     }
@@ -27,12 +30,18 @@ contract FarcasterResolver is SchemaResolver {
     /// @param attestation The attestation to revoke
     function onRevoke(Attestation calldata attestation, uint256 /*value*/) internal override returns (bool) {
        (uint256 fid, address verifyAdrress,) = abi.decode(attestation.data, (uint256, address, uint8));
-        if (verifyAdrress != fidAttested[fid]) {
+        bytes32 key = computeKey(fid, verifyAdrress);
+        if (attestation.uid != fidAttested[key]) {
             return false;
         }
 
-        delete fidAttested[fid];
+        delete fidAttested[key];
 
         return true;
+    }
+
+
+    function computeKey(uint256 _fid, address _verifyAddr) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_fid, _verifyAddr));
     }
 }
