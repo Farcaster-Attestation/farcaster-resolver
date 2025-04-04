9ddd4150a3b996aedbc51ff0c23af3e706ba9b85
Chomtana <Chomtana001@gmail.com>
2025-04-04 05:15:13 +0100

    Update supersim deployment

--- deploy-supersim.sh
+++ deploy-supersim.sh
@@ -1,6 +1,8 @@
 rm -rf ignition/deployments/supersim-10
 rm -rf ignition/deployments/supersim-8453
 
+supersim fork --chains op,base --interop.autorelay
+
 npx hardhat ignition deploy ignition/modules/FarcasterResolver.ts --network supersim_op --deployment-id supersim-10
 npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_op --deployment-id supersim-10 --parameters ./ignition/parameters/supersim.json --strategy create2
 npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_base --deployment-id supersim-8453 --parameters ./ignition/parameters/supersim.json --strategy create2
