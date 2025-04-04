3888cfbac414a67a8c5fbf7db7590210bc943318
Chomtana <Chomtana001@gmail.com>
2025-04-04 05:02:33 +0100

    Add OP mainnet deployment

--- deploy-supersim.sh
+++ deploy-supersim.sh
@@ -1,6 +1,6 @@
-rm -rf ignition/deployments/chain-10
-rm -rf ignition/deployments/chain-8453
+rm -rf ignition/deployments/supersim-10
+rm -rf ignition/deployments/supersim-8453
 
-npx hardhat ignition deploy ignition/modules/FarcasterResolver.ts --network supersim_op
-npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_op --parameters ./ignition/parameters/supersim.json --strategy create2
-npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_base --parameters ./ignition/parameters/supersim.json --strategy create2
+npx hardhat ignition deploy ignition/modules/FarcasterResolver.ts --network supersim_op --deployment-id supersim-10
+npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_op --deployment-id supersim-10 --parameters ./ignition/parameters/supersim.json --strategy create2
+npx hardhat ignition deploy ignition/modules/FarcasterResolverExtended.ts --network supersim_base --deployment-id supersim-8453 --parameters ./ignition/parameters/supersim.json --strategy create2
